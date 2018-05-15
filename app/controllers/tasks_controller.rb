b503b2fe5f21ae81e0a194f63e9dccbd9c66d3d3
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-15 15:53:00 +0200

    Added the new message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,17 +7,35 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
   def edit
   end
 
   def update
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Read All
-  get "tasks", to:"tasks#index"
+  get "tasks", to:"tasks#index", as: :tasks
   # Create
-  get "tasks", to:"tasks#new"
+  get "tasks/new", to:"tasks#new", as: :new
   post "tasks", to:"tasks#create"
   # Read One
   get "tasks/:id", to:"tasks#show", as: :task
   # Update
-  get "tasks/:id/edit", to:"tasks#edit"
+  get "tasks/:id/edit", to:"tasks#edit", as: :edit
   patch "tasks/:id", to:"tasks#update"
   # Destroy
-  delete "tasks/:id", to:"tasks#destroy"
-
+  delete "tasks/:id", to:"tasks#destroy", as: :delete
 
 
 end
