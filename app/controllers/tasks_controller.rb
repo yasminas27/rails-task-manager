cb16cd30b23b4ab23ce7eb6756d09ab9495ee82b
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-15 12:21:50 +0200

    suite exo 1

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+ before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read All
+  get "tasks", to:"tasks#index"
+  # Create
+  get "tasks", to:"tasks#new"
+  post "tasks", to:"tasks#create"
+  # Read One
+  get "tasks/:id", to:"tasks#show"
+  # Update
+  get "tasks/:id/edit", to:"tasks#edit"
+  patch "tasks/:id", to:"tasks#update"
+  # Destroy
+  delete "tasks/:id", to:"tasks#destroy"
+
+
+
 end
